b97e182e7bb1059b53da3b77ae4d2c8c892ad802
valeriu.mandeal <valeriu.mandeal@gmail.com>
2021-10-18 15:29:01 +0300

    Initial

---
+++ DesignPatterns.Singleton.Naive/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton.Naive
+{
+    class Program
+    {
+        static void Main()  
+        {
+            // client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
---
+++ DesignPatterns.Singleton.Naive/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton.Naive
+{
+    // The Singleton class defines the `GetInstance` method that serves as an
+    // alternative to constructor and lets clients access the same instance of
+    // this class over and over.
+    public class Singleton
+    {
+        // The Singleton's instance is stored in a static field. 
+        private static Singleton _instance;
+
+        // The Singleton's constructor should always be private to prevent
+        // direct construction calls with the `new` operator.
+        private Singleton() {}
+
+        public static Singleton GetInstance()
+        {
+            if (_instance is null)
+                _instance = new Singleton();
+
+            return _instance;
+        }
+
+        public static void PerformHardcoreBusinessLogic()
+        {
+            Console.WriteLine("Performing hardcore business logic!");
+        }
+    }
+}
